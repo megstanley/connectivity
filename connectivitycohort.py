835945e94b6a10c1e9c2cfaaf864b2c2520e1864
megstanley <meganstanley4@gmail.com>
2018-10-30 17:56:30 -0700

    30102018

--- connectivitycohort.py
+++ connectivitycohort.py
@@ -6,14 +6,14 @@ class ConnectivityCohort():
 
     def __init__(self, filename, here_dir = None):
         self.filename = filename
-        self.here_dir = here_dir
+        self.here_dir = os.path.dirname(os.path.realpath('__file__'))
 
     def find_data_path(self):
         '''
         Returns path of the data
         '''
-        self.here_dir    = os.path.dirname(os.path.realpath('__file__'))
-        self.dataset_dir = os.path.join(self.here_dir, 'data')
+        self.par_dir = os.path.abspath(os.path.join(self.here_dir, os.pardir))
+        self.dataset_dir = os.path.join(self.par_dir, 'spectral_decomposition-master/data')
         self.filepath = (self.dataset_dir + '/' + self.filename)
 
     def load_matrices(self):
@@ -23,3 +23,26 @@ class ConnectivityCohort():
         data_shape = self.data.shape
         self.cohort_size = data_shape[2]
         self.connectome_size = data_shape[0]
+
+    def cohort_statistics(self, p):
+        self.mean = np.mean(self.data, axis = 2)
+        self.var = np.var(self.data, axis = 2)
+        self.zeromean = np.subtract(self.data, self.mean)
+
+
+
+    # def add_brains(self, matrix):
+    #     '''add additional brain matrices to cohort, from file or matrix'''
+    #     #check matrix has the right size
+    #     if matrix.size == False:
+    #         print('Need input matrix')
+    #         return
+    #     data_shape = matrix.shape
+    #
+    #     if data_shape[0:2] == self.data.shape[0:2]:
+    #         self.data = np.append(self.data, matrix, axis = 2)
+    #     else:
+    #         print('Matrix has incorrect dimensions')
+    #         return
+    #     data_shape = self.data.shape
+    #     self.cohort_size = data_shape[2]
--- connectivitymatrix.py
+++ connectivitymatrix.py
@@ -1,12 +1,25 @@
 import numpy as np
-
+from scipy.io import loadmat
+import os
 
 
 class ConnectivityMatrix():
-    
 
-    def __init__(self, data):
-        self.connectome_number = data.shape[2]
-        self.connectome_size = data.shape[0]
+    def __init__(self, Cmat):
+        self.connectome_size = Cmat.shape[0]
+        self.connectome = Cmat
 
-    def load_matrices(data):
+    def get_adjacency(self):
+        '''calculate the adjacency matrix from the connectivity,
+        returns 1 if a connection is present'''
+        self.adjacency = (self.connectome > 0).astype(int)
+        rowdiag = np.diag(np.sum(self.adjacency, axis = 0))
+        coldiag = np.diag(np.sum(self.adjacency, axis = 1))
+        self.degree = np.sqrt(rowdiag*coldiag) #matrix with degrees of nodes on the diagonal
+
+    def get_laplacian(self):
+        '''calculate a normalised Laplacian matrix from the connectivity'''
+        self.rowsum = np.sum(self.connectome, axis = 0)
+        self.colsum = np.sum(self.connectome, axis = 1)
+        self.norm = np.sqrt(self.rowsum*self.colsum).reshape(self.connectome_size) #making normalization vector
+        self.laplacian = np.identity(self.connectome_size) - np.divide(self.connectome, self.norm)
